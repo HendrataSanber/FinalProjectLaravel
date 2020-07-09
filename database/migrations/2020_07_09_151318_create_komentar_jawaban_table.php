3f025e4cfb7279810cf4bcaadab643ae34c0a7a0
HendrataSanber <hendrata.loekman@gmail.com>
2020-07-10 04:55:59 +0700

    perbaikan migration

---
+++ database/migrations/2020_07_09_151318_create_komentar_jawaban_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKomentarJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('komentar_jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('isi');
+            $table->timestamps();
+            $table->unsignedBigInteger('jawaban_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('jawaban_id')->references('id')->on('jawaban');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('komentarjawaban');
+    }
+}
