0e8f1c11717b9bf3ce471e69adf8e580faa013fe
denyhanif <36994300+denyhanif@users.noreply.github.com>
2020-07-12 12:14:48 +0700

    blade show

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -18,41 +18,51 @@
                 </div>
             </div>
         </div>
-        <div class="card-body mt-2">
-            <h6 class="mb-4">{{ $pertanyaan[0]->created_at }}</h6>
-            <?php echo htmlspecialchars_decode($pertanyaan[0]->isi); ?>
-            <br>
-            <a href="#" class="btn btn-info btn-icon-split mt-5">
-                <span class="icon text-white-50">
-                    <i class="fas fa-info-circle"></i>
-                </span>
-                <span class="text">{{ $pertanyaan[0]->tag }}</span>
-            </a>
-        </div>
-        <div>
-            Total poin:{{$pertanyaan[0]->totalvote}}
-        </div>
-        <?php
-            if($pertanyaan[0]->currentvote==10){
-                echo "User ini sudah upvote";
-            }
-            else if($pertanyaan[0]->currentvote==-1){
-                echo "User ini sudah downvote";
-            }
-            else{
-                echo "User ini belum vote";
-            }
-        ?>
-        <a href="/upvotep/{{$pertanyaan[0]->id}}">
-                Up
-        </a>
-        <a href="/downvotep/{{$pertanyaan[0]->id}}">
-                Down
-        </a>
+        <div class="card-body mt-1">
+            <div class="row align-self-md-center">
+                <div class="col-2  ">
+                    
+                        
+                        <a class="btn btn-info mb-2 mt-1 ml-5" href="/upvotep/{{$pertanyaan[0]->id}}">
+                            <i class="fa fa-arrow-alt-circle-up "></i> 
+                        </a>
+                        <br>
+                        <h3 class="text-info ml-4 mt-2 pl-4" > {{$pertanyaan[0]->totalvote}}</h3>
+                        <a class="btn btn-info mt-2 ml-5 mb-3" href="/downvotep/{{$pertanyaan[0]->id}}">
+                            <i class="fa fa-arrow-alt-circle-down "></i> 
+                        </a>
+                        <br>
+                        <div class=" text-center">
+                            
+                            <?php if($pertanyaan[0]->currentvote==10){
+                            echo '<h6 class="text-success"> User ini sudah upvote</h6>';}
+                            else if($pertanyaan[0]->currentvote==-1){
+                            echo '<h6 class="text-danger"> User ini sudah downvote</h6> ';
+                            }else{
+                            echo  '<h6 class="text-warning"> User ini belum upvote</h6>';
+                            }
+                        ?>
+                        </div>
+                </div>
+                <div class="col-10 border-left-info">
+                    <h6 class="mb-4">{{ $pertanyaan[0]->created_at }}</h6>
+                    <?php echo htmlspecialchars_decode($pertanyaan[0]->isi); ?>
+                    <br>
+                    <a href="#" class="btn btn-info btn-icon-split mt-5">
+                        <span class="icon text-white-50">
+                        <i class="fas fa-info-circle"></i>
+                        </span>
+                        <span class="text">{{ $pertanyaan[0]->tag }}</span>
+                    </a>
+                </div>
+            </div>
         <hr>
+        </div>
+        
         @foreach($pertanyaan[0]->komentar as $item)
         
         <div class="card shadow ml-3 mr-3 my-1 border-left-info w-50">
+            
             
             <blockquote class="blockquote pt-1 pl-3 text-info">&quot;
                 {{$item["isi"]}} &quot;
@@ -80,31 +90,33 @@
     <div class="card shadow m-4">
         <div class="card-body mt-2">
             <h6 class="mb-4">{{ $item->created_at }}</h6>
-            <?php echo htmlspecialchars_decode($item->isi); ?>
-            <br>
+            <div class="card" style="height: 20%"><?php echo htmlspecialchars_decode($item->isi); ?>
         </div>
-        <hr>
-        <div>
-            Total poin:{{$item->totalvote}}
-            <br>
+        <div class="row mt-4 ml-2 d-flex">
+            <a  class=" btn btn-info" href="/upvotej/{{$item->id}}/{{$pertanyaan[0]->id}}">
+                <i class="fas fa-arrow-up"></i>
+            </a>
+            <h6 class="text-info my-2 mx-4 ">{{$item->totalvote}}</h6>
+            <a  class=" btn btn-info" href="/downvotej/{{$item->id}}/{{$pertanyaan[0]->id}}">
+            <i class="fas fa-arrow-down"></i>
+            </a>
+        </div>
+           
             <?php
                 if($item->currentvote==10){
-                    echo "User ini sudah upvote";
+                    echo '<h6 class="text-success">User ini sudah upvote</h6>';
                 }
                 else if($item->currentvote==-1){
-                    echo "User ini sudah downvote";
+                    echo '<h6 class="text-danger">User ini sudah downvote</h6>';
                 }
                 else{
-                    echo "User ini belum vote";
+                    echo '<h6 class="text-danger">User ini belum vote</h6>';
                 }
             ?>
-        </div>
-        <a href="/upvotej/{{$item->id}}/{{$pertanyaan[0]->id}}">
-                Up
-        </a>
-        <a href="/downvotej/{{$item->id}}/{{$pertanyaan[0]->id}}">
-                Down
-        </a>
+        <hr>
+    </div>
+        
+        
         @foreach($item->komentar as $item2)
          <div class="card shadow ml-3 mr-3 my-1 border-left-info w-50">
             
@@ -132,12 +144,15 @@
         </div>
     </div>
     @endforeach
-    <h1>Form kosong untuk jawaban baru disini:</h1><br>
     <form action="/jawaban" method="POST">
     @csrf
-        <textarea name="isi" placeHolder="isi disini"></textarea>
-        <br>
+    <div class="card shadow m-5 p-2">
+        <h6>Jawaban baru</h6>
+        <textarea class=" ckeditor mx-5"name="isi" placeHolder="Masukkan jawaban baru di sini"></textarea>
         <input type="hidden" name="pertanyaan_id" value="{{$pertanyaan[0]['id']}}">
-        <input type="submit">
+        <div class="d-block text-center pt-2">        <input  class=" btn btn-success d-flex"type="submit">    
+</div>
+    </div>
+     
     </form>
 @endsection
